90c2a6de0d487feaf45ddd530791bcf9f56637f5
lizzieinvancouver <wolkovich@biodiversity.ubc.ca>
2013-12-11 16:05:41 -1000

    Adjusted G to gmax and d to u.

--- R/PhenologyModel.r
+++ R/PhenologyModel.r
@@ -9,6 +9,20 @@ options(stringsAsFactors=FALSE)
 # packages
 library(ggplot2)
 
+
+#set.seed(2)
+
+# define all parameters
+nsp <- 20    # number of spp
+nyrs <- 100  # number of yrs 
+ndays <- 1  # number of days in a growing season
+dt <- 0.001 # within yr timestep
+y <- c(1:nyrs)
+tsteps <- ndays/dt
+
+## Extinction Threshold:  1 seed per hectare (assuming that initial density is 10 seeds per meter)
+ext <- 1/10000
+
 # set up graphics parameters
 colerz <- topo.colors(nsp)
 rcol <- "darkslateblue"
@@ -16,19 +30,6 @@ linez <- rep(1:6, 100) # enough for 600 species for now
 lspbyrs <- 1
 lresbyrs <- 2
 lwd=2
-
-#set.seed(2)
-
-# define all parameters
-nsp <- 20    # number of spp
-nyrs <- 100  # number of yrs
-ndays <- 2  # number of days in a growing season
-dt <- 0.001 # within yr timestep
-y <- c(1:nyrs)
-tsteps <- ndays/dt
-
-## Extinction Threshold:  1 seed per hectare (assuming that initial density is 10 seeds per meter)
-ext <- 1/10000
 
 ## Setting up loop for multiple model runs
 # To do still:
@@ -46,10 +47,10 @@ for (j in c(1:nruns)){ # assuming, we will vary species characteristics between 
 b <-  rep(1,nsp)          # biomass of seedling
 s <-  rep(0.8,nsp)      # seedbank survival overwinter
 a <-  rep(20,nsp)        # slope of species uptake rate with increasing R
-d <-  rep(1,nsp)          # inverse of the max uptake rate
+u <-  rep(1,nsp)          # inverse of the max uptake rate
 c <-  rep(12,nsp)        # conversion of resource to biomass
 m <-  rep(0.05,nsp)     # mortality
-G <-  rep(0.5,nsp)     # max germination fraction
+gmax <-  rep(0.5,nsp)     # max germination fraction
 h <-  rep(100,nsp)             # max rate of germination decrease following pulse
 phi <- rep(0.05,nsp)     # conversion of end-of-season plant biomass to seeds
 tauI <- runif(nsp,0.1, 0.9)    # time of max germ for sp i
@@ -58,7 +59,7 @@ theta <- rep(1,nsp)         # shape of species i uptake curve
 N0 <- rep(10,nsp)          # initial number of seeds (per meter square?)
 Rstar <- (m/(a*(c-m*d)))^(1/theta)
 
-crossyrsvars <- as.data.frame(cbind(b, s, a, d, c, m, G, h, phi, theta))
+crossyrsvars <- as.data.frame(cbind(b, s, a, u, c, m, gmax, h, phi, theta))
 
 ##
 ## time-varying env variables
@@ -66,12 +67,12 @@ crossyrsvars <- as.data.frame(cbind(b, s, a, d, c, m, G, h, phi, theta))
 mu <- log(2)  #mean of resource distribution
 sigma <- 0.2  #sd of resource distribution
 R0 <- rlnorm(nyrs, mu, sigma) # intial R in a season
-
 eps <- 1              # evaporative stress
 #tauP <- 0.3           # timing of pulse
 p <- 2  #first parameter for beta distribution of tau
 q <- 2  #second parameter for beta distribution of tau
-tauP <- rbeta(nyrs,p,q)
+tauP <- rbeta(nyrs,p,q) 
+
 ##
 ## Within-growing season dynamics set-up
 ##
@@ -81,7 +82,7 @@ B <- array(rep(0), dim=c(nyrs,nsp,tsteps)) # where B is an array with yr (nyrs),
 N <- matrix(rep(0), nyrs, nsp) # number of seeds by yr and spp
 N[1,] <- N0  #initialize
 Bfin <- matrix(rep(0),nyrs,nsp) # biomass at end of year y
-
+E <- matrix(rep(0),nyrs,nsp)
 
 ##
 ## change to mapply someday?
@@ -90,18 +91,20 @@ Bfin <- matrix(rep(0),nyrs,nsp) # biomass at end of year y
 ##
 
 for (y in c(1:(nyrs-1))){
-  g <- G*exp(-h*(tauP[y]-tauI)^2)  #germination fraction in year y
+  g <- gmax*exp(-h*(tauP[y]-tauI)^2)  #germination fraction in year y
   k<-1
   R[y,k] <- R0[y]
   B[y,,k] <- b*g*N[y,]
   while (R[y,k]>min(Rstar)){
-      f <- (a*R[y,k]^theta)/(1+a*d*R[y,k]^theta)
+      f <- (a*R[y,k]^theta)/(1+a*u*R[y,k]^theta)
       B[y,,k+1] <- B[y,,k]+(c*f-m)*B[y,,k]*dt
       R[y,k+1] <- R[y, k] -dt*(t(B[y,,k]) %*% f + eps*R[y,k])
       R[y,k+1] <- R[y,k+1]*(R[y,k+1]>0)
       k <- k+1
     }
   Bfin[y,] <- apply(B[y,,], 1, max)  #final biomass
+  E[y,] <- log(s*g*phi)+log(B[y,,1]*((1+a*u*R0[y]^theta)/
+      (1+a*u*R0[y]^theta*exp(-eps*ndays*theta)))*exp((-c/(u*eps*theta))-m*ndays))
   N[y+1,] <- s*(N[y,]*(1-g)+phi*Bfin[y,])  #convert biomass to seeds and overwinter
   N[y+1,] <- N[y+1,]*(N[y+1,]>ext)  #if density does not exceed ext, set to zero
 }
@@ -109,6 +112,7 @@ for (y in c(1:(nyrs-1))){
 ## here, below is the bit that needs more thought
 # could also make each run a list, so something like:
 # modelruns[[y]] <- list(crossyrsvars, Bfin)
+
 modelruns[[paste("crossyrs", j, sep="")]] <- crossyrsvars
 modelruns[[paste("withinyrs", j, sep="")]] <- Bfin
 }
