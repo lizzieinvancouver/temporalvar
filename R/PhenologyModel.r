30cb82f56c831d25b21ebd2fd52abdb0ca593a90
Megan Donahue <donahuem@hawaii.edu>
2014-01-04 15:33:27 -1000

    big changes:  added ODE solver, pulled out sections and put in source files, restructured within year arrays
    There are a lot of changes here.  The first two are the big ones:
    1) I pulled out the paramterization sections into separate source files.
    These source files were added in a separate commit (6 source files:
    getSpecies, getEnvt, getGraphParms, getRunParms, as well as two ode
    solver files: NoCompN and ResCompN)
    2) I added the ode solver to the loop.  This caused some other changes
    because the output from the ode solver is a dataframe with elements
    time,R, B1,B2....  I saved teh dataframe for each year as an element in
    a list.  Ultimately, this may use too much memory, but it was useful for
    now.
    3.  I commented out the larger loop whie I was working on the annual
    loop
    4. I fixed the first couple of figures generating bits to work with the
    new within-year data structure,but  didn't finish that fix.  I have
    marked where I left off, and it shoudln't be too hard to figure out how
    to fix.
    5. I changed how we handled the extra time for a nonstationary run.  If
    it is a nonstationary run, then nonsta is assigned a value >0 that is
    the number of years of nonsttionarity.  This is then used as a flag to
    adjust the parameter values in other places.  Thereofre, we cna just
    switch the flag (from 0 to number of nonstationary years) to to
    stationary or nonstationary runs.  Hope that will work.
    
    Sorry for all the different things that got changed here... poor coding
    style/

--- R/PhenologyModel.r
+++ R/PhenologyModel.r
@@ -11,28 +11,6 @@ library(ggplot2)
 
 #set.seed(2)
 
-# define all parameters
-nsp <- 20    # number of spp
-nyrs1 <- 100  # number of yrs to run first stationary period
-ndays <- 20  # number of days in a growing season
-dt <- 0.01 # within yr timestep
-tsteps <- ndays/dt
-# params for adding on a nonstationary run after stationary run
-nyrs2 <- 0 # number of yrs for second run (nonstationary for now)
-nyrs <- nyrs1+nyrs2 # ALERT! change below once not doing stationary+nonstationary run
-y <- c(1:nyrs)
-
-## Extinction Threshold:  1 seed per hectare (assuming that initial density is 10 seeds per meter)
-ext <- 1/10000
-
-# set up graphics parameters
-colerz <- topo.colors(nsp)
-rcol <- "darkslateblue"
-linez <- rep(1:6, 100) # enough for 600 species for now
-lspbyrs <- 1
-lresbyrs <- 2
-lwd=2
-
 ## Setting up loop for multiple model runs
 # To do still:
 # (1) add in everything crossyrsvars need (was lazy about this)
@@ -44,109 +22,77 @@ lwd=2
 #nruns <- 2 # number of model runs to do
 #for (j in c(1:nruns)){ # assuming, we will vary species characteristics between yrs ... 
   
+#Stationarity in this run?
+nonsta = 0  #flag for stationary (0) vs nonstationary (=num yrs nonstationary)
 
-## species characteristics
-##
-b <-  rep(1,nsp)          # biomass of seedling
-s <-  rep(0.8,nsp)      # seedbank survival overwinter
-a <-  rep(20,nsp)        # slope of species uptake rate with increasing R
-u <-  rep(1,nsp)          # inverse of the max uptake rate
-c <-  rep(12,nsp)        # conversion of resource to biomass
-theta <- rep(1,nsp)         # shape of species i uptake curve, remember it should be an integer!
-m <-  rep(0.05,nsp)     # mortality
-gmax <-  rep(0.5,nsp)     # max germination fraction
-h <-  rep(100,nsp)             # max rate of germination decrease following pulse
-phi <- rep(0.05,nsp)     # conversion of end-of-season plant biomass to seeds
-tauI <- runif(nsp,0.1, 0.9)    # time of max germ for sp i
-# set up tracking (to do for Lizzie, there should be code under here someday)
+#Number of species to start?
+nsp = 2  #when nsp=2, tauI is assigned known values from chesson 2004
 
+source("getRunParms.R") #define runtime parameters
 
-N0 <- rep(10,nsp)          # initial number of seeds (per meter square?)
-Rstar <- (m/(a*(c-m*u)))^(1/theta)
+source("getGraphParms.R")  #define graphics parameters
 
-crossyrsvars <- as.data.frame(cbind(b, s, a, u, c, m, gmax, h, phi, theta, tauI, Rstar))
+source("getSpecies.R")  #get species characteristics and Rstar
 
-##
-## time-varying env variables
-##
-mu <- log(2)  #mean of resource distribution
-sigma <- 0.2  #sd of resource distribution
-R0 <- rlnorm(nyrs, mu, sigma) # intial R in a season
-eps <- 1              # evaporative stress 
-#tauP <- 0.3           # timing of pulse
-p <- 2  #first parameter for beta distribution of tau
-q <- 2  #second parameter for beta distribution of tau
-tauPs <- rbeta(nyrs1, p, q) # change once not doing stationary+nonstationary run
+source("getEnvt.R")  #get constant and time-varying envt parms
 
-# nonstationary tauP, change once not doing stationary+nonstationary run
-qns <- seq(2, 20, length.out=nyrs2)
-tauPns <- rbeta(nyrs2, p, qns) # yes, it takes a vector! Yay!
-plot(tauPns~c(1:50))
-tauP <- c(tauPs, tauPns)
-
-##
-## Within-growing season dynamics set-up
-##
-R <- matrix(rep(0), nyrs, tsteps) # R is the resource level through the growing season (each yr)
-B <- array(rep(0), dim=c(nyrs,nsp,tsteps)) # where B is an array with yr (nyrs), spp biomass
-    # through growing season (ndays)
+#Define arrays
+#interannual dynamics set-up (R0 is in getEnvt.R)
+N0 <- rep(100,nsp)          # initial number of seeds (per meter square?)
 N <- matrix(rep(0), nyrs, nsp) # number of seeds by yr and spp
 N[1,] <- N0  #initialize
-Bfin <- matrix(rep(0),nyrs,nsp) # biomass at end of year y
-B0  <- matrix(rep(0),nyrs,nsp) # biomass at beginning of year y
 rcrt <- matrix(rep(0),nyrs,nsp) # recruitment in year y
 
-##define function for ODE solver
-ResCompN <- function(Time,Inits,Pars) {
-  with(as.list(c(State,Pars)),{
-    dB = (c*a*R^theta/(1+a*u*R^theta)-m)*B
-    dR  = -eps*R - sum(B*a*R^theta/(1+a*u*R^theta))
-    return(list(c(dB,dR)))
-  })
-}
-Pars <- c(c=rep(12,nsp),a=rep(20,nsp),u=rep(1,nsp),m=rep(0.05,nsp),theta=rep(1,nsp),eps=1)
-Time <- seq(0,ndays,by=dt)
+## Within-season dynamics set-up
+Bfin <- matrix(rep(0),nyrs,nsp) # biomass at end of year y
+B0  <- matrix(rep(0),nyrs,nsp) # biomass at beginning of year y
+Bout <- list() #each year has a dataframe with time,R(t),Bi(t) of dims(2+nsp,tsteps)
+source("ResCompN.R") # define within-season ode solver
+source("NoCompN.R")  # define within-season ode solver for no competition
 
-##
 ## set-up for different coexistence mechanisms
-##
-tauIstar <- matrix(rep(0),nyrs,nsp)
+BnoCout <- list()
 BnoC <- matrix(rep(0),nyrs,nsp) # B without competition at end of year y
 E <- matrix(rep(0),nyrs,nsp)
 C <- matrix(rep(0),nyrs,nsp)
 
-##
-## Better to use ODE solver within each year?
-##
-
 for (y in c(1:(nyrs-1))){
   g <- gmax*exp(-h*(tauP[y]-tauI)^2)  #germination fraction in year y
-  k<-1
-  R[y,k] <- R0[y]
-  B[y,,k] <- b*g*N[y,]
-  Inits <- c(N=seq(1,12,by=1),R=100) 
-  out <- as.data.frame(ode(func = ResCompN, y = Inits, parms = Pars, times = Time))
-  while (R[y,k] > min(Rstar)){
-      f <- (a*R[y,k]^theta)/(1+a*u*R[y,k]^theta)
-      B[y,,k+1] <- B[y,,k]+(c*f-m)*B[y,,k]*dt
-      R[y,k+1] <- R[y, k] -dt*(t(B[y,,k]) %*% f + eps*R[y,k])
-      R[y,k+1] <- R[y,k+1]*(R[y,k+1]>0)
-      k <- k+1
-    }
-  Bfin[y,] <- apply(B[y,,], 1, max)  #final biomass
-  rcrt[y+1,] <- s*g*(phi*Bfin[y,]-1)   #to recruit, convert biomass to seeds and overwinter
+  #get initial biomass for year y
+  B0[y,] <- b*g*N[y,]
+  #use deSolve for ResCompN
+  R<-R0[y]
+  B<-B0[y,]
+  State<-c(R=R,B=B)
+  Time <- seq(0,ndays,by=dt)
+  Bout[[y]] <- as.data.frame(ode(func = ResCompN, y = State, parms = Pars, times = Time))
+  #matplot(Bout[[y]]$time,Bout[[y]][,-1],type="l",ylim=c(0,2.5))
+  #legend("right",names(Bout[[y]])[2:(nsp+2)],lty = 1:5,col=1:6)
+  Bfin[y,] <-  apply(Bout[[y]][3:(2+nsp)],2,FUN=max)  #final biomass
+  
+  #use deSolve for NoCompN to solve for noCompetition condition
+  #would be faster to used solved equation, but calculations were coming out wrong
+  tstar <- (1/eps)*(log(R0[y]) - (1/theta)*log(m/(a*c-a*u*m)))  
+  TimeNC <- seq(0,max(tstar)+5*dt,by=dt)
+  BnoCout[[y]] <- as.data.frame(ode(func = NoCompN, y = State, parms = Pars, times = TimeNC))
+  BnoC[y,] <- apply(BnoCout[[y]][3:(2+nsp)],2,FUN=max)
+  #matplot(BnoCout[[y]]$time,BnoCout[[y]][,-1],type="l")
+  #legend("right",names(BnoCout[[y]])[2:(nsp+2)],lty = 1:5,col=1:6)
+  
+  rcrt[y,] <- s*g*(phi*Bfin[y,]-1)   #to recruit, convert biomass to seeds and overwinter
   
   #calculate E and C
-  #tstar is the end of the season when there is no competition
-  tstar <- (1/eps)*(log(R0[y]-(1/theta)*log(m/(a*c-a*d*m))))
-  e1 <- 1+a*d*R0[y]^theta
-  e2 <- 1+a*d*R0[y]^theta*exp(-eps*tstar*theta)
-  e3 <- c/d/eps/theta
-  BnoC[y,] <- B0[y,] * e1^(-e3) * e2^(-e3) * exp(-m*tstar)
+  #tstar is when species cross their Rstar threshold and we stop the season under the no competition condition
+  #tstar is correct: checked against ODE solver, it is when the  biomass starts decreasing
+  #BUT BnoC still doesn't make sense
+  #e1 <- 1+a*u*R0[y]^theta
+  #e2 <- 1+a*u*R0[y]^theta*exp(-eps*tstar*theta)
+  #e3 <- c/(u*eps*theta)
+  #BnoC[y,] <- B0[y,] * e1^(-e3) * e2^(-e3) * exp(-m*tstar)
   E[y,] <- s*g*(phi*BnoC[y,]-1)      #the -1 accounts for the loss of adults to germination
   C[y,] <- E[y,]/rcrt[y,]
 
-  N[y+1,] <- s*(N[y,]) + rcrt[y+1,]    #N(t+1) = survival + recruitment
+  N[y+1,] <- N[y,]*(s + rcrt[y,])    #N(t+1) = N(t)* (survival + recruitment)
   N[y+1,] <- N[y+1,]*(N[y+1,]>ext)  #if density does not exceed ext, set to zero
 }
 
@@ -171,13 +117,15 @@ for (i in 1:nsp) {
 }
 
 dev.new(width=14, height=10)
-range=c(1:(tsteps/3))
-plot(R[1,range]~range, ylim=c(min(R), max(R)),
+#range=c(1:(tsteps/3))
+plot(Bout[[1]]$R~Bout[[1]]$time, ylim=c(0, max(R0)),
      xlab="step, step, step", ylab="Resource", type="n")
-for (i in 2:nyrs) {
-  lines(R[i,range]~range, col=colerz[i], lty=lspbyrs, lwd=lwd)
+#hmm, something I did messed up the colors so I had to leave that out
+for (i in 1:(nyrs-1)) {
+  lines(Bout[[i]]$R~Bout[[i]]$time)#, col=colerz[i], lty=lspbyrs, lwd=lwd)
 }
 
+###Megan stopped tweaking plots here, but they will need to be adjusted for new within-year output structure from ode
 
 # within years plots
 # tweaked a little to make biomass differences among years clearer
