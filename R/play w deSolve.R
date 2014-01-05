346e1399a733f097990681aa15077f17da4b648b
Megan Donahue <donahuem@hawaii.edu>
2014-01-04 15:26:06 -1000

    figuring out syntax
    I was using this file to figure out the syntax for calling the ode
    solver

--- R/play w deSolve.R
+++ R/play w deSolve.R
@@ -4,8 +4,8 @@ library(deSolve)
 
 LotVmod <- function (Time, State, Pars) {
   with(as.list(c(State, Pars)), {
-    dx = x*(alpha - beta*y)
-    dy = -y*(gamma - delta*x)
+    dx <- x*(alpha - beta*y)
+    dy <- -y*(gamma - delta*x)
     return(list(c(dx, dy)))
   })
 }
@@ -29,30 +29,54 @@ ResComp2 <- function(Time,State,Pars) {
     return(list(c(dN1,dN2,dR)))
   })
 }
-Pars <- c(c1=12,c2=12,a1=20,a2=20,u1=1, u2=1,m1=.05,m2=.05,theta1=1,theta2=1,eps=1)
+Pars <- c(c1=12,c2=12,a1=10,a2=20,u1=1, u2=1,m1=.05,m2=.05,theta1=1,theta2=1,eps=1)
 State <- c(N1=10,N2=20,R=log(2))
 Time <- seq(0,1000,by=.1)
 
-out <- as.data.frame(ode(func = ResComp2, y = State, parms = Pars, times = Time))
+outResComp2 <- as.data.frame(ode(func = ResComp2, y = State, parms = Pars, times = Time))
 
-matplot(out[,-1], type = "l", xlab = "time", ylab = "population")
+matplot(outResComp2[,-1], type = "l", xlab = "time", ylab = "population")
 
 
 #try N consumers, 1 resource
-nsp = 12
-ResCompN <- function(Time,Inits,Pars) {
+nsp = 20
+ndays=3
+dt=.0001
+eps=1
+source("getSpecies.R")
+source("ResCompN.R")
+# ResCompN <- function(Time,State,Pars) {
+#   with(as.list(c(State,Pars)),{
+#     dR  = -eps*R - sum(B*a*R^theta/(1+a*u*R^theta))
+#     dB = (c*a*R^theta/(1+a*u*R^theta)-m)*B
+#     return(list(c(dR,dB)))
+#   })
+# }
+#Pars <- c(c=c,a=a,u=u,m=m,theta=theta,eps=eps)
+R0=20
+B0=c(rep(0,10), seq(5,10,length.out=10))
+B=B0
+State <- c(R=R0,B=B)
+#Time <- seq(0,3,by=.1)
+outResCompN <- as.data.frame(ode(func = ResCompN, y = State, parms = Pars, times = Time))
+matplot(Time, outResCompN[,-1],type = "l", xlab = "time", ylab = "population")
+
+#No competition model
+NoCompN <- function(Time,State,Pars) {
   with(as.list(c(State,Pars)),{
     dN = (c*a*R^theta/(1+a*u*R^theta)-m)*N
-    dR  = -eps*R - sum(a*R^theta/(1+a*u*R^theta))
+    dR  = -eps*R
     return(list(c(dN,dR)))
   })
 }
-Pars <- c(c=rep(12,nsp),a=rep(20,nsp),u=rep(1,nsp),m=rep(0.05,nsp),theta=rep(1,nsp),eps=1)
-Inits <- c(N=seq(1,12,by=1),R=100)
-Time <- seq(0,25,by=.1)
-
-out <- as.data.frame(ode(func = ResCompN, y = Inits, parms = Pars, times = Time))
-
-matplot(out[,-1], type = "l", xlab = "time", ylab = "population")
-
+source("getSpecies.R")
+eps=1
+R0=2
+tstar <- (1/eps)*(log(R0)-(1/theta)*log(m/(a*c-a*u*m)))
+Pars <- c(c=c,a=a,u=u,m=m,theta=theta,eps=eps)
+N=seq(1,20,1)
+State <- c(N=N,R=R0)
+Time <- seq(0,max(tstar)+.01,by=.01)
+outNoCompN <- as.data.frame(ode(func = NoCompN, y = State, parms = Pars, times = Time))
+matplot(Time, out[,-1],type = "l", xlab = "time", ylab = "population")
 ##ooooh! it works!
