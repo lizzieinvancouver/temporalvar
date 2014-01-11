18c965764d74f995cb965e59ac07cd817c59506a
Megan Donahue <donahuem@hawaii.edu>
2014-01-11 12:38:37 -1000

    Added rootfun to stop integration.  Probably don't need it

--- R/NoCompN.R
+++ R/NoCompN.R
@@ -9,3 +9,11 @@ NoCompN <- function(Time,State,Pars) {
   })
 }
 Pars <- c(c=c,a=a,u=u,m=m,theta=theta,eps=eps)
+
+##add rootfunction so that the integration stops once (opt1) R is numerically 0 (threshold hh) 
+rootfunNoC<-function(Time, State, Pars) {
+  dstate <- unlist(ResCompN(Time, State, Pars))
+  #opt 1. this works, makes threshold when R is 0
+  hh <- abs(dstate[1]) - 1e-35  
+  return(hh)
+}
