6036b401cb7810e2065da5b3eefd0867b1010a4c
Megan Donahue <donahuem@hawaii.edu>
2014-01-11 12:39:24 -1000

    #added variable iyr, so can be plotted excluded initial transient years

--- R/plotNyears.R
+++ R/plotNyears.R
@@ -1,21 +1,21 @@
 #Between Years Plots
-
+iyr = 20  #initial year in plot, to remove transient
 # between years plot
 par(mfrow=c(1,1))
 dev.new(width=14, height=10)
 par(mar=c(5,4,4,5)+.1)
-plot(Bfin[,1]~c(1:nyrs), ylim=c(min(Bfin), max(Bfin)),
+plot(Bfin[iyr:nyrs,1]~c(iyr:nyrs), ylim=c(0, max(Bfin[iyr:nyrs])),
      xlab="year", ylab="Abundance", type="n")
 for (i in 1:nsp) {
-  lines(Bfin[,i]~c(1:nyrs), col=colerz[i], lty=lspbyrs, lwd=lwd)
+  lines(Bfin[iyr:nyrs,i]~c(iyr:nyrs), col=colerz[i], lty=lspbyrs, lwd=lwd)
 }
 #overlay germination fraction
 par(new=TRUE)
-plot(g[,i]~c(1:nyrs),type="n",xaxt="n",yaxt="n",xlab="",ylab="")
+plot(g[iyr:nyrs,i]~c(iyr:nyrs),type="n",xaxt="n",yaxt="n",xlab="",ylab="")
 axis(4)
 mtext("germination fraction (+)",side=4,line=3)
 for (i in 1:nsp) {
-  points(g[,i]~c(1:nyrs),col=colerz[i],pch="+")
+  points(g[iyr:nyrs,i]~c(iyr:nyrs),col=colerz[i],pch="+")
 }
 #OVerlays Resource level, R0, each year
 # par(new=TRUE)
