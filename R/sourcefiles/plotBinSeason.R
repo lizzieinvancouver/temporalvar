d2e420c9d9642786edd020b58e037d89017f79b9
donahuem <donahuem@hawaii.edu>
2014-09-15 13:46:31 -1000

    corrected the max value for ylim so the biomass would plot correctly

--- R/sourcefiles/plotBinSeason.R
+++ R/sourcefiles/plotBinSeason.R
@@ -2,12 +2,13 @@
 
 dev.new(width=14, height=10)
 par(mfrow=c(3,3))
+
 for (i in 1:length(plotyrs)){
   q=plotyrs[i]
-  plot(Bout[[q]][,3]~Bout[[q]]$time, ylim=c(0, max(Bfin[plotyrs,])),
+  plot(Bout[[q]][,3]~Bout[[q]]$time, ylim=c(0, max(Bout[[q]][3:(nsp+2)])),
        xlab="step, step, step", ylab="Biomass", type="n",main=plotyrs[i])
   for (j in 1:nsp){
-    lines(Bout[[q]][,2+j]~Bout[[q]]$time, col=colerz[j], lty=lspbyrs, lwd=lwd)
+    lines(Bout[[q]][,2+j]~Bout[[q]]$time,ylim=c(0, max(Bout[[q]][3:(nsp+2)])), col=colerz[j], lty=lspbyrs, lwd=lwd)
   }
   #overlay R on a second y axis
   par(new=TRUE)
