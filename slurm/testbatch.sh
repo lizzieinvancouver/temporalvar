3cff51851664d7f6e557a4b5d5c4e6ed56e18d2a
donahuem <donahuem@hawaii.edu>
2016-03-12 12:33:21 -0500

    testing arrays in SLURM

---
+++ slurm/testbatch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash                                                                                               
+
+#SBATCH --job-name=PhenTest_20160312  #
+
+#SBATCH --output=PhenTest_20160312-%A-%a.out  # Standard out goes to this file
+
+#SBATCH --error=PhenTest_20160312-%A-%a.err		  # Standard err goes to this file                                          
+
+#SBATCH -n 1 # Number of cores requested                                                                 
+
+#SBATCH -N 1 # Ensure that all cores are on one machine                                                  
+
+#SBATCH -t 60 # Runtime in minutes                                                                       
+
+#SBATCH -p wolkovich # Partition to submit to (mine!)                                                    
+
+#SBATCH --mem=1000 # Memory per cpu in MB (see also --mem-per-cpu)                                       
+
+#SBATCH --mail-type=END
+ 
+#SBATCH --mail-user=donahuem@hawaii.edu<
+ 
+source new-modules.sh
+module load R_packages
+
+R CMD BATCH --quiet --no-restore --no-save PhenologyModel.R
+
+###run this with the following command 
+
+###sbatch --array-1-5 testbatch.sh
+
+###where 5 is the number of jobs in the array
+###where testbatch.sh is the name of the batch script
