7479953a4ee3da5012a289299c03dcf27c4de5c2
donahuem <donahuem@hawaii.edu>
2016-03-12 13:01:44 -0500

    updates to slurm array test files

--- R/batchtest.R
+++ R/batchtest.R
@@ -6,6 +6,6 @@ evar <- Sys.getenv(c("SLURM_ARRAY_JOB_ID","SLURM_ARRAY_TASK_ID"))
 print(evar)
 
 for (i in c(1:5)){
-  #save(evar,file=paste("output/testbatch_host-",evar[1],"_psize-",evar[2],"_iter", i,".Rdata",sep=""))
-  save(evar,file=paste("output/testbatch_jobid-",evar[1],"_taskid-",evar[2],"_iter", i,".Rdata",sep="")) 
+  #save(evar,file=paste("R/output/testbatch_host-",evar[1],"_psize-",evar[2],"_iter", i,".Rdata",sep=""))
+  save(evar,file=paste("R/output/testbatch_jobid-",evar[1],"_taskid-",evar[2],"_iter", i,".Rdata",sep="")) 
 }
