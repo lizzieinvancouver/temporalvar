d75e844e0092200b45ff4b2eb340b28a434be70f
donahuem <donahuem@hawaii.edu>
2016-06-13 02:49:21 -1000

    add slurm file for concatenation

---
+++ slurm/concatPhenOut.sh
@@ -0,0 +1,34 @@
+#!/bin/bash                                                                                               
+
+#SBATCH --job-name=concatPhenOut  #
+
+#SBATCH --output=concatPhenOut-%A-%a.out  # Standard out goes to this file
+
+#SBATCH --error=concatPhenOut-%A-%a.err		  # Standard err goes to this file                                          
+
+#SBATCH -n 1 # Number of cores requested                                                                 
+
+#SBATCH -N 1 # Ensure that all cores are on one machine                                                  
+
+#SBATCH -t 10 # Runtime in minutes                                                                       
+
+#SBATCH -p wolkovich # Partition to submit to (mine!)                                                    
+
+#SBATCH --mem=1000 # Memory per cpu in MB (see also --mem-per-cpu)                                       
+
+#SBATCH --mail-type=END
+ 
+#SBATCH --mail-user=donahuem@hawaii.edu<
+ 
+source new-modules.sh
+module load R_packages
+
+R CMD BATCH --quiet --no-restore --no-save R/sourcefiles/concatPhenologyOut.r
+
+###run this with the following command from the temporalvar folder
+
+
+
+###sbatch slurm/concatPhenOut.sh
+
+###where concatPhenOut.sh is the name of the batch script
