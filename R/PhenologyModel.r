6c1b7a69643826186f9ce2a7ac795625cd05bf14
donahuem <donahuem@hawaii.edu>
2018-01-30 02:26:35 -0700

    updated sourcing, relative paths, and order of events
    In addition to updating the sourcing and relative paths, the meaningful
    change here was to re-order when N and B are updated.  This allows the
    data to be output each year more sensibly.  Now, years count from 1:nyrs
    and all of the variables are calculated in year y from year y-1 values.
    In addition, this means that the order of events is:  (1) rcrt(y) is the
    number of seeds being added in y that were genereated in y-1, (2) count
    the seeds in year y as the prior year (less germ adn overwinter)
    +rcrt(y), (3) within year dynamics generate Bout[y] and Bfin[y].

--- R/PhenologyModel.r
+++ R/PhenologyModel.r
@@ -2,57 +2,78 @@
 ### By Lizzie & Megan ###
 ### This executes a single run of the model and writes out
 
+rm(list=ls()) 
+options(stringsAsFactors=FALSE)
+
 #define run conditions
-source("R/sourcefiles/getRunParms.R") #define runtime parameters
+loc = "C:/Users/Megan/Documents/GitHub/temporalvar/R/"  #"/n/wolkovich_lab/temporalvar/R/"
+source(paste0(loc, "/sourcefiles/getRunParms.R")) #define runtime parameters
 
 for (j in c(1:nruns)){
   
   #define parameters and functions for this run
-  source("R/sourcefiles/getEnvt.R")  #get constant and time-varying envt parms
-  source("R/sourcefiles/getSpecies.R")  #get species characteristics and Rstar
-  source("R/sourcefiles/ResCompN.R") # define within-season ode solver
+  source(paste0(loc,"/sourcefiles/getEnvt.R"))  #get constant and time-varying envt parms
+  source(paste0(loc,"/sourcefiles/getSpecies.R"))  #get species characteristics and Rstar
+  source(paste0(loc,"/sourcefiles/ResCompN.R")) # define within-season ode solver
   
   #Define arrays
   #interannual dynamics set-up (R0 is in getEnvt.R)
   N0 <- rep(100,nsp)          # initial number of seeds (per meter square?)
-  N <- matrix(rep(0), nyrs, nsp) # number of seeds by yr and spp
-  N[1,] <- N0  #initialize
-  rcrt <-  matrix(rep(0),nyrs,nsp) # recruitment in year y
+  N <- matrix(rep(-1), nyrs, nsp) # number of seeds by yr and spp
+  rcrt <- matrix(rep(-1), nyrs, nsp) # number of rcrts by yr and spp
+  coexist <- matrix(rep(-1),nyrs,nsp)
   
   ## Within-season dynamics set-up
-  
-  Bfin <- matrix(rep(0),nyrs-1,nsp) # biomass at end of year y
+  Bfin <- matrix(rep(0),nyrs,nsp) # biomass at end of year y
   B0  <- matrix(rep(0),nyrs,nsp) # biomass at beginning of year y
   Bout <- list() #each year has a dataframe with time,R(t),Bi(t) of dims(2+nsp,tsteps)
+  yout <- nyrs  #last year of output; default is nyrs, unless loop breaks early
   
   ## And away we go!
-  for (y in c(1:(nyrs-1))){
+  for (y in yrs){
     #include a flag for runs with initial and final stationary periods, but no nonstationary period
     if (y==(nonsta[1]+1) && nonsta[2]==0) N[y,] <- N0 #if no ns period, reset for final stationary period
     #get initial biomass for year y
+    if (y==1) {
+      N[y,] <- N0
+      rcrt[y,] <- N0
+      coexist[y,] <- (N[y,]>0)*1
+      } 
+    else {
+      rcrt[y,] <- phi*Bfin[y-1,]*s  #number of seeds added from last year's growth
+      N[y,]<- N[y-1,]*(1-g[y-1,])*s + rcrt[y,]  #note Bfin already includes N(t) as init cond; USES g here!
+      N[y,] <- N[y,]*(N[y,]>ext)  #if density does not exceed ext, set to zero
+      coexist[y,] <- (N[y,]>0)*1
+      if (!(sum(N[y,]>0))) {
+        yout <- y  #if break the loop, then y-1 is last year before extinction
+        #give Bout a value for the last iteration
+        Bout[[y]] <- Bout[[y-1]][1,]*0  #get list formatting from prior year
+        Bout[[y]][1:4] <- c(0,R0[y],b*g[y,]*N[y,])
+        Bfin[y,] <- Bout[[y]][3:(2+nsp)]
+        #print("All species have gone extinct, go to next run")
+        break    #if all species have gone extinct, go to next run
+      }
+    }
     B0[y,] <- b*g[y,]*N[y,] 
     #use deSolve for ResCompN
     R<-R0[y]
     B<-B0[y,]
     State<-c(R=R,B=B)
     Time <- seq(0,ndays,by=dt)
+    #set Rstarmin threshold & update rootfun; this accounts for case where the spp with min R* goes extinct
+    Rstarmin <- min(Rstar[(N[y,]!=0)])
+    rootfun <- function(Time, State, Pars) State[1] -Rstarmin
+
     Bout[[y]] <- as.data.frame(ode(func = ResCompN, y = State, parms = Pars, times = Time,
                                    rootfun=rootfun))
     #Bout[[y]] <- as.data.frame(lsodar(func = ResCompN, y = State, parms = Pars, times = Time,rootfun=rootfun))
     Bfin[y,] <-  apply(Bout[[y]][3:(2+nsp)],2,FUN=max)  #final biomass
-    N[y+1,] <- N[y,]*s*(1-g[y,]) + phi*Bfin[y,]    #note Bfin already includes N(t) as init cond; USES g here!
-    N[y+1,] <- N[y+1,]*(N[y+1,]>ext)  #if density does not exceed ext, set to zero
-    if (!(sum(N[y+1,]>0))) break    #if all species have gone extinct, go to next run
-    rcrt[y,] <- g[y,]*(phi*Bfin[y,]-s)   #to recruit, convert biomass to seeds and overwinter
-    #set Rstarmin threshold & update rootfun; this accounts for case where the spp with min R* goes extinct
-    Rstarmin <- min(Rstar[(N[y+1,]!=0)])
-    rootfun <- function(Time, State, Pars) State[1] -Rstarmin
   }
-  
   ## modelruns includes the variables that are constant across years in one dataframe...
   # then tauI, tauP and Bfin for each year
-  save(sppvars, envtvars,tauIhat,Bfin,g, file=paste("R/output/",runname,"_", jobID[1],"-",jobID[2],"-run",j,".Rdata",sep="")) 
-  if (writeBout>0 && j%%writeBout==0) {
-    save(Bout,file=paste("R/output/",runname,"_Bout_",jobID[1],"-",jobID[2],"-run",j,".Rdata",sep="")) #("out_",i,".Rdata"))
-  }
+  source(paste0(loc,"/sourcefiles/getOutput.R"))
+  #save(sppvars, envtvars,tauIhat,Bfin,g,runparms, file=paste(outloc,runname,"_", jobID[1],"-",jobID[2],"-run",j,".Rdata",sep="")) 
+  #if (writeBout>0 && j%%writeBout==0) {
+  #  save(Bout,file=paste(outloc,runname,"_Bout_",jobID[1],"-",jobID[2],"-run",j,".Rdata",sep="")) #("out_",i,".Rdata"))
+  #}
 }
