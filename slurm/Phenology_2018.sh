a7e90a006e93f0dcd8567817ad23ad56e5f09492
donahuem <donahuem@hawaii.edu>
2018-09-25 05:41:15 -0400

    made edits suggested by RC:  load R rather than R_packages; remove new-modules.sh; remove spaces between SBATCH calls; fix / in mkdir directory location; add export R_USER_LIBS command.

--- slurm/Phenology_2018.sh
+++ slurm/Phenology_2018.sh
@@ -1,28 +1,18 @@
 #!/bin/bash                                                                                               
-
 #SBATCH --job-name=PhenologyModel-%A-%a  #
-
 #SBATCH --output=RunTimeOut/%A-%a.out  # Standard out goes to this file
-
-#SBATCH --error=RunTimeOut/%A-%a.err		  # Standard err goes to this file                                          
-#SBATCH -n 1 # Number of cores requested                                                                 
-
+#SBATCH --error=RunTimeOut/%A-%a.err		  # Standard err goes to this file                                   #SBATCH -n 1 # Number of cores requested                                                                 
 #SBATCH -N 1 # Ensure that all cores are on one machine                                                  
-
 #SBATCH -t 600 # Runtime in minutes                                                                       
-
 #SBATCH -p wolkovich # Partition to submit to (mine!)                                                    
-
 #SBATCH --mem=1000 # Memory per cpu in MB (see also --mem-per-cpu)                                       
-
 #SBATCH --mail-type=END
- 
 #SBATCH --mail-user=donahuem@hawaii.edu<
  
-source new-modules.sh
-module load R_packages
-
 mkdir -p /scratch/wolkovich_lab/temporalvar/$SLURM_JOB_ID
+
+module load R
+export R_LIBS_USER=$HOME/apps/R:$R_LIBS_USER
 
 Rscript /n/wolkovich_lab/temporalvar/R/PhenologyModel.R --quiet --no-restore --no-save 
 
