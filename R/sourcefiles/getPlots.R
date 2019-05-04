c10e1d51b32155597a958108fcc85f1a3ee86cdb
donahuem <donahuem@hawaii.edu>
2019-05-03 17:22:02 -0700

    generalize plots for nonstationary periods of different lengths

--- R/sourcefiles/getPlots.R
+++ R/sourcefiles/getPlots.R
@@ -15,10 +15,10 @@ col_2=rgb(1,0,0,.5)
 
 ##Print out overlapping histograms of tauIP for sp1 and sp 2 for stationary and nonstationary
 
-h1t1=hist(tauP-tauIhat[seq(1,500),1],plot=FALSE)
-h2t1=hist(tauP-tauIhat[seq(1,500),2],plot=FALSE)
-h1t2=hist(tauP-tauIhat[seq(501,1000),1],plot=FALSE)
-h2t2=hist(tauP-tauIhat[seq(501,1000),2],plot=FALSE)
+h1t1=hist(tauP-tauIhat[seq(1,nonsta[1]),1],plot=FALSE)
+h2t1=hist(tauP-tauIhat[seq(1,nonsta[1]),2],plot=FALSE)
+h1t2=hist(tauP-tauIhat[seq(nonsta[1]+1,sum(nonsta)),1],plot=FALSE)
+h2t2=hist(tauP-tauIhat[seq(nonsta[1]+1,sum(nonsta)),2],plot=FALSE)
 ymax=max(h1t1$counts,h2t1$counts,h1t2$counts,h2t2$counts)
 xmin=min(h1t1$breaks,h2t1$breaks,h1t2$breaks,h2t2$breaks)
 xmax=max(h1t1$breaks,h2t1$breaks,h1t2$breaks,h2t2$breaks)
@@ -35,10 +35,10 @@ legend("topleft",legend=c("Sp1","Sp2"), col=c(col_1,col_2),lty=1,lwd=2,bty="n")
 
 ##Print out overlapping histograms of germination for sp1 and sp 2 for stationary and nonstationary
 
-g1t1=hist(g[seq(1,500),1],plot=FALSE)
-g2t1=hist(g[seq(1,500),2],plot=FALSE)
-g1t2=hist(g[seq(501,1000),1],plot=FALSE)
-g2t2=hist(g[seq(501,1000),2],plot=FALSE)
+g1t1=hist(g[seq(1,nonsta[1]),1],plot=FALSE)
+g2t1=hist(g[seq(1,nonsta[1]),2],plot=FALSE)
+g1t2=hist(g[seq(nonsta[1]+1,sum(nonsta)),1],plot=FALSE)
+g2t2=hist(g[seq(nonsta[1]+1,sum(nonsta)),2],plot=FALSE)
 
 ymax=max(g1t1$counts,g2t1$counts,g1t2$counts,g2t2$counts)
 xmin=min(g1t1$breaks,g2t1$breaks,g1t2$breaks,g2t2$breaks)
