edfedbe7b2e136fccff355d18600f0e52ff809a2
donahuem <donahuem@hawaii.edu>
2020-07-24 10:08:13 -0600

    adding file back after reset

--- R/graphs/conceptual/ConceptualFig_Tracking.R
+++ R/graphs/conceptual/ConceptualFig_Tracking.R
@@ -6,22 +6,21 @@
 #     measurement filter (what researchers measure)
 # inset somewhere showing correlation between ideal day of event v predicted day of event = cue quality
 # 
+
+# Libraries Etc -----------------------------------------------------------
+
 rm(list=ls())
 library(tidyverse)
 library(dplyr)
 library(lubridate)
 library(zoo)
-setwd("C:/Users/Megan/Documents/GitHub/temporalvar/R")
-
-# get environmental data from NEON
-# load packages
 library(neonUtilities)
-#library(geoNEON)
 library(raster)
 library(rhdf5)
-# Set global option to NOT convert all character variables to factors
+library(gridExtra)
+library(RColorBrewer)
 options(stringsAsFactors=F)
-
+setwd("C:/Users/Megan/Documents/GitHub/temporalvar/R")
 
 # get Neon Data -----------------------------------------------------------
 
@@ -44,15 +43,13 @@ if(file.exists("neondata/neon.Rdata")) {
   # irbt <- loadByProduct(dpIDs[1],site=sites,
   #                       startdate=startD, enddate=endD,
   #                       nCores=2)
-  # par <- loadByProduct(dpIDs[2],site=sites,
-  #                      startdate=startD, enddate=endD,
-  #                      nCores=2)
+  # par <- loadByProduct(dpIDs[2],site="ABBY",
+  #                      startdate=startD, enddate=endD)
   # precip <- loadByProduct(dpIDs[3],site=sites,
   #                         startdate=startD, enddate=endD,
   #                         nCores=2)
   # swc <- loadByProduct(dpIDs[4],site=sites,
-  #                      startdate=startD, enddate=endD, avg=30,
-  #                      nCores=2)
+  #                        startdate=startD, enddate=endD, avg=30)
   # airt <- loadByProduct(dpIDs[5],site=sites,
   #                       startdate=startD, enddate=endD,
   #                       nCores=2)
@@ -72,7 +69,7 @@ if(file.exists("neondata/neon.Rdata")) {
   bioT <- read_csv("neondata/NEON_temp-bio/stackedFiles/IRBT_30_minute.csv")
   airT <- read_csv("neondata/NEON_temp-air-single/stackedFiles/SAAT_30min.csv")
   
-  save(bioT, precip1, precip2, airT, file="neondata/neon.Rdata")
+  save(bioT, precip1, precip2, airT, par, swc, file="neondata/neon.Rdata")
 }
 
 # Make dataframe ----------------------------------------------------------
@@ -80,12 +77,14 @@ airT.thresh.lo <- 0
 airT.thresh.hi <- 32
 bioT.thresh.lo <- 0
 bioT.thresh.hi <- 32
+swc.thresh.lo <- 0.5
 
 d <- 
   left_join(
     airT %>% 
       filter(verticalPosition=="010") %>% 
       mutate(lt.lo = if_else(tempSingleMean<airT.thresh.lo,1,0),
+             gt.lo = if_else(tempSingleMean>airT.thresh.lo,1,0),
              gt.hi = if_else(tempSingleMean>airT.thresh.hi,1,0)) %>% 
       group_by(site = siteID, 
                date = date(startDateTime)) %>%
@@ -93,12 +92,15 @@ d <-
                 airT.min = ifelse(sum(!is.na(tempSingleMean))>0,min(tempSingleMean,na.rm=TRUE),NA),
                 airT.max = ifelse(sum(!is.na(tempSingleMean))>0,max(tempSingleMean,na.rm=TRUE),NA),
                 airT.lt.lo = ifelse(sum(!is.na(lt.lo))>0, sum(lt.lo, na.rm=TRUE)*0.5,NA),
+                airT.gt.lo = ifelse(sum(!is.na(gt.lo))>0, sum(gt.lo, na.rm=TRUE)*0.5,NA),
                 airT.gt.hi = ifelse(sum(!is.na(gt.hi))>0, sum(gt.hi, na.rm=TRUE)*0.5,NA),
                 airT.dch = ifelse(sum(!is.na(lt.lo))>0, sum(lt.lo*0.5 * (airT.thresh.lo - tempSingleMean), na.rm=TRUE),NA),
-                airT.dhh = ifelse(sum(!is.na(gt.hi))>0, sum(gt.hi*0.5 * (tempSingleMean - airT.thresh.hi), na.rm=TRUE),NA),
+                airT.dhh.hi = ifelse(sum(!is.na(gt.hi))>0, sum(gt.hi*0.5 * (tempSingleMean - airT.thresh.hi), na.rm=TRUE),NA),
+                airT.dhh.lo = ifelse(sum(!is.na(gt.lo))>0, sum(gt.lo*0.5 * (tempSingleMean - airT.thresh.lo), na.rm=TRUE),NA),
                 airT.ct = sum(!is.na(tempSingleMean))) %>%  
-      mutate(airT.dhh.5d = rollapplyr(airT.dhh, 5, sum, na.rm=TRUE, fill=NA)) %>% 
-      mutate(airT.dch.90d = rollapplyr(airT.dch, 90, sum, na.rm=TRUE, fill=NA)),
+      mutate(airT.dhh.5dH = rollapplyr(airT.dhh.hi, 5, sum, na.rm=TRUE, fill=NA),
+             airT.dch.90d = rollapplyr(airT.dch, 90, sum, na.rm=TRUE, partial=TRUE),
+             airT.dhh.30dL = rollapplyr(airT.dhh.lo,30,sum,na.rm=TRUE,partial=TRUE)),
     bioT %>%  
       filter(verticalPosition == "010") %>% 
       mutate(lt.lo = if_else(bioTempMean<bioT.thresh.lo, 1, 0),
@@ -122,31 +124,63 @@ d <-
                date = date(startDateTime)) %>% 
       summarise(precip.daily = ifelse(sum(!is.na(secPrecipBulk))>0,sum(secPrecipBulk, na.rm = TRUE),NA),
                 precip.daily.any = any(secPrecipBulk>0)) %>% 
-      mutate(precip.cum.15d = rollapplyr(precip.daily, 15, sum, na.rm=TRUE, fill=NA)) %>% 
-      mutate(precip.cum.30d = rollapplyr(precip.daily, 30, sum, na.rm=TRUE, fill=NA)),
-    by=c("date","site")) #%>% 
-  # left_join(.,
-  #           swc %>% 
-  #             group_by(site = siteID, 
-  #                      date = date(startDateTime)) %>% 
-  #             summarise(swc.mean = mean(VSWCMean), 
-  #                       swc.min = mean(VSWCMinimum),
-  #                       swc.max = mean(VSWCMaximum)),
-  #           by=c("date","site")) %>% 
-  # left_join(.,
-  #         soilt %>% 
-  #           group_by(site = siteID, 
-  #                    date = date(startDateTime)) %>% 
-  #           summarise(soilT.mean = mean(soilTempMean), 
-  #                     soilT.min = mean(soilTempMinimum),
-  #                     soilT.max = mean(soilTempMaximum)),
-  #         by=c("date","site"))  
+      mutate(precip.cum.15d = rollapplyr(precip.daily, 15, sum, na.rm=TRUE, fill=NA),
+             precip.cum.30d = rollapplyr(precip.daily, 30, sum, na.rm=TRUE, fill=NA)),
+    by=c("date","site")) %>% 
+  left_join(.,
+    par %>%
+      group_by(siteID, startDateTime) %>% 
+      summarise(PARMeanV = mean(PARMean,na.rm=TRUE)) %>% 
+      ungroup %>% 
+      group_by(site = siteID,
+               date = date(startDateTime)) %>%
+      summarise(par.cum = ifelse(sum(!is.na(PARMeanV))>0, sum(PARMeanV,na.rm=TRUE),NA)) %>% 
+      mutate(par.7d = rollapplyr(par.cum, 7,mean,na.rm=TRUE, partial=TRUE)), 
+    by=c("date","site")) %>% 
+  left_join(.,
+    swc %>%
+      filter(verticalPosition == "501") %>%
+      group_by(siteID, startDateTime) %>% 
+      summarise(VSWCMeanH = mean(VSWCMean,na.rm=TRUE),
+                VSWCMinH = min(VSWCMean,na.rm=TRUE),
+                VSWCMaxH= max(VSWCMean, na.rm=TRUE)) %>% 
+      ungroup %>% 
+      mutate(lt.lo = if_else(VSWCMeanH < swc.thresh.lo,1,0)) %>%
+      group_by(site = siteID,
+               date = date(startDateTime)) %>%
+      summarise(swc.mean = mean(VSWCMeanH, na.rm=TRUE),
+                swc.min = ifelse(sum(!is.na(VSWCMinH))>0,min(VSWCMinH,na.rm=TRUE),NA),
+                swc.max = ifelse(sum(!is.na(VSWCMaxH))>0,max(VSWCMaxH,na.rm=TRUE),NA),
+                swc.lt.lo = ifelse(sum(!is.na(lt.lo))>0, sum(lt.lo, na.rm=TRUE)*0.5,NA),
+                swc.ct = sum(!is.na(VSWCMeanH))),
+    by=c("date","site"))
 
-rm(airT,bioT,precip1,precip2)
+rm(airT,bioT,precip1,precip2,swc,par)
 
 # basic plots -------------------------------------------------------------
 
-#use grid.arrange
+xax <- c(mdy("11/1/2017"),
+         mdy("12/1/2017"),
+         mdy("1/1/2018"),
+         mdy("2/1/2018"),
+         mdy("3/1/2018"),
+         mdy("4/1/2018"),
+         mdy("5/1/2018"),
+         mdy("6/1/2018"),
+         mdy("7/1/2018"),
+         mdy("8/1/2018"),
+         mdy("9/1/2018"),
+         mdy("10/1/2018"))
+
+# xax <- c(mdy("11/1/2017"),
+#          mdy("1/1/2018"),
+#          mdy("3/1/2018"),
+#          mdy("5/1/2018"),
+#          mdy("7/1/2018"),
+#          mdy("9/1/2018"))
+xaxL <-c("N","D","J","F","M","A","M","J","J","A","S","O")
+  
+
 d %>% 
   group_by(site) %>% 
   ggplot() +
@@ -177,35 +211,223 @@ d %>%
   group_by(site) %>% 
   ggplot() +
   facet_wrap(vars(site)) +
+  geom_line(aes(x=date,y=par.cum,color=site)) +
+  labs(x = "Day",y="Cum. Daily PAR")
+d %>% 
+  group_by(site) %>% 
+  ggplot() +
+  facet_wrap(vars(site)) +
+  geom_line(aes(x=date,y=swc.mean)) +
+  geom_line(aes(x=date,y=swc.min),color="light blue") +
+  geom_line(aes(x=date,y=swc.max),color="pink") +
+  labs(x = "Day",y="Daily Soil Water Content")
+d %>% 
+  group_by(site) %>% 
+  ggplot() +
+  facet_wrap(vars(site)) +
   geom_point(aes(x=bioT.mean,y=airT.mean))
+d %>% 
+  group_by(site) %>% 
+  ggplot() +
+  facet_wrap(vars(site)) +
+  geom_point(aes(x=swc.mean,y=precip.cum.30d))
 
-
-# Fitness -----------------------------------------------------------------
-
-#for fitness calc:
-# growth is a unimodal function of temperature
-# P(success) is 0 if subsequent freezing exceeds 4 hours, and declines with degree heating hours in excess of 40deg 
-#growth increments depend on daily temperature
-grT.aa <- .2
-grT.min <- 15
-grT.max <- 35
-t <- seq(0,40,.2)
-growth.T <- ifelse(between(t,15,35), grT.aa * t * (t - grT.min) * (grT.max - t)^(1 / 2),0)
-plot(t,growth.T)
+# Add Growth, Survival, Fitness -----------------------------------------------------------------
+#gives fitness at each date as a function of:
+#airT.lt.lo = hours of air temperature below the low threshold each day (e.g., freezing)
+#airT.dhh.5dH = degree heating hours above upper threshold (e.g., 35C) in 5 days
+#bioT.mean = mean daily "biological" temperature (based on IR)
+#par.cum = cumulative daily par
+#swc.mean = mean daily soil water content
+gr.Taa <- .2
+gr.Tmin <- 15
+gr.Tmax <- 35
+gr.PARb <- .0002  #rate of decline in growth multiplier w increasing PAR (growht increases with PAR to max = 1)
+gr.swcA <- 500  #logistic curve for effect of swc
+gr.swcB <- 120
 #survival depends on cumulative cold or heat stress
-cumFr <- 4  #excess of 4 hours below freezing after EVENT
-cumHe <- 48  #48 degree heating hours that exceed 48
-surv.T <- ifelse(airT.lt0>4,0,ifelse(airT.gt40>)
-surv.T <- rT.aa * t * abs((rT.min-t) * (t-rT.max))^(1 / 2)
-plot(t,surv.T)
-rSM[d] <- 
+s.LTa <- 0.45  #rate of decline of surv with increase hours below freezing
+s.HTa <- 0.07  #rate of decline of surv with increase DHH>35 in 5d
+season = 30
+
+dd<- d %>%
+  filter(date%within%interval(ymd("2018-1-01"),ymd("2018-10-31")) , site =="ABBY"|site=="MOAB") %>%
+  mutate(growth.T = ifelse(between(bioT.mean,15,35),gr.Taa*bioT.mean*(bioT.mean-gr.Tmin)*(gr.Tmax-bioT.mean)^(1/2),0),
+         growth.PAR = (1-exp(-gr.PARb*par.cum)),
+         growth.SWC = (1/(1 + gr.swcA*exp(-gr.swcB*swc.mean))),
+         growth.day = growth.T/max(growth.T,na.rm=TRUE) * growth.PAR/max(growth.PAR,na.rm=TRUE) * growth.SWC/max(growth.SWC,na.rm=TRUE)) %>% 
+  mutate(surv =  1*exp(-s.LTa*airT.lt.lo)*exp(-s.HTa*airT.dhh.5dH),
+         surv.n = surv/max(surv,na.rm=TRUE),
+         fit.day = growth.day * surv) %>% 
+  group_by(site) %>% 
+  mutate(surv.start=rollapply(surv, season, prod, na.rm=TRUE, fill=NA, align="left"),
+         growth.start = rollapply(growth.day,season,sum,na.rm=TRUE,fill=NA,align="left"),
+         fit.start = surv.start*growth.start,
+         fit.start.n = fit.start/max(fit.start,na.rm=TRUE))
+# Add Cue Function -----------------------------------------------------
+
+#Degree chilling hours over 90 days - airT.dch.90d
+#Degree heating hours avobe freezing over 30 days - airT.dhh.30dL
+# Need cheating hours to come *after* chilling hour threshold is met
+
+dd %>% 
+  ggplot(aes(x=date,y=airT.dch.90d, color=site)) +
+  geom_line(size=1.5)+
+  labs(x="", y="Degree Chilling Hours") +
+  scale_x_date(date_labels = "%b", breaks=xax)
+
+dd %>% 
+  ggplot(aes(x=date,y=airT.dhh.30dL, color=site)) +
+  geom_line(size=1.5)+
+  labs(x="", y="Degree Heating Hours") +
+  scale_x_date(date_labels = "%b", breaks=xax)
+
+#Create probability thresholds for degree heating * degree cooling
+dh.a = 3000
+dh.b = 1/1000
+dc.a = 500
+dc.b = 15 #scale by thousands of cooling hours
+
+dd<-dd %>%
+  group_by(site) %>% 
+  mutate(airT.dch.90d.n = airT.dch.90d/max(airT.dch.90d,na.rm=TRUE),
+         pEvent.dc = 1/(1 + dc.a*exp(-dc.b*airT.dch.90d.n)),
+         pEvent.dh = 1/(1 + dh.a*exp(-dh.b*airT.dhh.30dL)),
+         pEvent.day = (pEvent.dc)*pEvent.dh,
+         pNoEvent.day = 1-pEvent.day,
+         pEvent.by = 1-rollapply(pNoEvent.day,30,prod, na.rm=TRUE,partial=TRUE),
+         pEvent.by2 = cummax(pEvent.by))
+
+
+# Measurement Filter ------------------------------------------------------
+
+dd<-dd %>% 
+  group_by(site) %>% 
+  mutate(airT.30d = rollapply(airT.mean,30,mean,na.rm=TRUE, partial=TRUE,align="right"),
+         airT.max.30d = cummax(airT.30d)) 
+
+# Figure - Fitness ----------------------------------------------------------
+
+Fig.Fitness <- 
+  dd %>% 
+  ggplot(aes(x=date, y=fit.start)) +
+  geom_line(aes( color=site),size=1.5)+
+  labs(x="", y="Fitness") +
+  scale_color_brewer(palette="Dark2") +
+  theme_minimal() +
+  theme(axis.ticks.y = element_blank(),
+        axis.text.y = element_blank(),
+#        axis.text.x = element_blank(),
+        legend.position = "none") +
+  scale_x_date(date_labels = xaxL, breaks=xax)
+Fig.Fitness
+
+# Figure - Event Cue ------------------------------------------------------
+
+Fig.EventCue <-
+  dd%>%
+  ggplot(aes(x=date,y=pEvent.by2, color=site)) +
+  geom_line(size=1.5)+
+  labs(x="", y="P(Event)") +
+  theme_minimal() +
+  theme(axis.ticks.y = element_blank(),
+        axis.text.y = element_blank(),
+#        axis.text.x = element_blank(),
+        legend.position = "none") +
+  scale_x_date(date_labels = xaxL, breaks=xax) +
+  scale_color_brewer(palette="Dark2")
+Fig.EventCue
+
+# Figure - Measurement Filter ---------------------------------------------
+
+Fig.Measurement <-
+  dd %>% 
+  mutate(airT.exceed = 1*cummax(airT.30d)>12) %>% 
+  ggplot(aes(x=date, color=site)) +
+  geom_line(aes(y=airT.30d), size=1,alpha=0.7) +
+  geom_line(aes(y=airT.exceed*25),size=1.5) +
+#  scale_y_continuous(sec.axis = sec_axis(~./25)) +
+  labs(x="", y="Temp > Threshold") +
+  theme_minimal() +
+  theme(legend.position = "none",
+        axis.text.y = element_blank()) +
+  scale_x_date(date_labels = xaxL, breaks=xax) +
+  scale_color_brewer(palette="Dark2")
+Fig.Measurement
+
+# #Plots of Environment in focal year ---------------------------------------------------
+
+Fig.DailyAirT <-
+  dd %>% 
+  ggplot(aes(x=date, y=airT.mean,color=site)) +
+  geom_line(size=1)+
+  labs(x="",y="Temperature") +
+  theme_minimal() +
+  theme(legend.position = "none",
+#        axis.text.x = element_blank(),
+        axis.text.y = element_blank()) +
+  scale_x_date(date_labels = xaxL, breaks=xax) +
+  scale_color_brewer(palette="Dark2")
+Fig.DailyAirT
+
+Fig.PAR7d <-
+  dd %>% 
+  ggplot(aes(x=date,y=par.7d,color=site)) +
+  theme_minimal()+
+  scale_color_brewer(palette="Dark2") +
+  geom_line(size=1) +
+  labs(x="", y="PAR") +
+  theme(legend.position = "none") +
+  theme(axis.text.y = element_blank()) +
+  scale_x_date(date_labels = xaxL, breaks=xax)
+Fig.PAR7d
+
+Fig.Precip <-
+  dd %>% 
+  ggplot(aes(x=date, y=precip.daily,color=site)) +
+  geom_line(size=1)+
+  labs(x="", y="Precipitation") +
+  theme_minimal() +
+  theme(axis.text.y = element_blank()) +
+  theme(legend.position = "none") +
+  scale_x_date(date_labels = xaxL, breaks=xax) +
+  scale_color_brewer(palette="Dark2")
+Fig.Precip
+
+dd %>% 
+  ggplot +
+  geom_line(aes(x=date,y=swc.mean,color=site)) +
+  labs(x="Day of Year",y="Mean Soil Moisture")
 
 
 
-ndays <- 180
-fit <- rep(1,ndays)
-for (d in seq(1,ndays,1)){
-  for (dd in seq(1,d,1)){
-  fit[dd]<- fit[dd] + rT[d]
-  }
-}
+  dd %>% 
+  ggplot +
+  geom_line(aes(x=date,y=precip.cum.15d, color=site))+
+  labs(x="Day of Year",y="Cum. Precipitation (15d)")
+
+dd %>% 
+  ggplot +
+  geom_line(aes(x=date,y=airT.lt.lo,color=site)) +
+  labs(x="Day of Year",y="Hours below Freezing")
+
+
+# Assemble Figure ---------------------------------------------------------
+
+
+Fig.Concept <- grid.arrange(Fig.DailyAirT,
+             Fig.PAR7d, 
+             Fig.Precip,
+             Fig.Fitness,
+             Fig.EventCue,
+             Fig.Measurement,
+             layout_matrix=cbind(c(1,2,3),c(4,5,6)))
+
+ggsave(filename="graphs/conceptual/Fig_ConceptTrack.pdf",plot=Fig.Concept)
+
+
+
+
+
+
+
