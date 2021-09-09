1a08fecb23b20ed5605bfb132f2e6a5ebbc0b844
donahuem <donahuem@hawaii.edu>
2021-09-09 10:33:29 -0600

    more work on functional forms.

--- R/checks/checkFunctionForms_priorityeffs.R
+++ R/checks/checkFunctionForms_priorityeffs.R
@@ -20,20 +20,26 @@
   
   
 #create functional forms for reltaionship betweeen:
-# tauD = day of highest germination for species i
+# tauI = day of highest germination for species i
 # h = variability of germination timing for species i
 # gmax = maximum germination rate for species i
 # gtot = total germination for species i  (integral of g from day 0 to day T)
 # d is day in germination season d = 1...D (e.g., D=30)
+#for illustration, create 5 "species"  
+  D <- 25
+  d <- seq(1,D,1)
+  nsp=5
+  g <- matrix(NA,nrow=nsp, ncol=D)
+  tauD = seq(1,20,length=nsp)
+  h = c(1,5,2,3,.5)
+  gmax = c(1,.2,.8,1.2,1)
+  plot(d,rep(max(gmax),D),type="n", ylim = c(0,1.25), ylab = "germination fraction",xlab = "day")
+  for (i in seq(1,5,1)) {
+    g[i,] <- gmax[i]*exp(-((d-tauI[i])^2)/h[i])  #germination fraction at time T
+    lines(d,g[i,],type="l", col=i)  
+  }
+  #Calculate G = total germination.  This is an important constraint
+ G <-  rowSums(g)
   
-  tauD = 5
-  D = 30
-  h = 10
-  d = seq(1,D,1)
-  gmax = 1
-  g <- gmax*exp(-((d-tauD)^2)/h)  #germination fraction at time T
-  plot(d,g,type="l")  
-  
-  
-  #next, need to make tauD, h, and/or gmax a function of envt
+  #next, need to make tauI, h, and/or gmax a function of envt
   
