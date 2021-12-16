9543d54551b2e278eabcb414fcf364e6702f89f1
donahuem <donahuem@hawaii.edu>
2021-12-16 09:57:50 -0700

    updated getSpecies for chill dependence durin g 12/16/21 meeting

--- R/sourcefiles/getSpecies.R
+++ R/sourcefiles/getSpecies.R
@@ -7,41 +7,24 @@
 #        tau_g = f2(xi) = tau_max - (tau_max/xi_tau(i))  tau_max is max delay same for all species
 #                                                        xi_tau(i) chill weeks that result in min tau_g for species i
 #germination
-#chilling and timing
-tau_max <- 30               #max delay in germination, same for all species; think more about value
-xi_tau <- runif(nsp,0,16)   #chill weeks for min delay - species specific 
-tau_g <- tau_max - (tau_max/xi_tau)
+#timing as a function of xi (=chill)
+tau_max <- 30               #max delay in germination, same for all species; think more about value (y-int of tau_g v chill)
+xi_tau <- runif(nsp,0,16)   #chill weeks for min delay - species specific (x-intercept of tau_g v chill)
+#tau_g is the delya i germination in year(y) defined by the line, where y=tau_g, yint = tau_max, slope=tau_max/xi_tau, and x=chill=xi
+tau_g <- tau_max - (tau_max/matrix(rep(xi_tau,each=nyrs),nrow=nyrs, ncol=nsp))*matrix(rep(xi,nsp),nrow=length(xi),ncol=nsp)
 
-#chilling and germ frac
+#germ frac as a function of xi (=chill)
 g0 <- runif(nsp,0,1)      #species-specific min germination, depending on chill
                           ##do we need to have a probability of a true 0 (won't get any real zeros with a runif)
 gmax <- c(runif(1,g0[1],1),
-          runif(1,g0[2],1) #species-specific max germination, depending on chill, must be larger than g0
+          runif(1,g0[2],1)) #species-specific max germination, depending on chill, must be larger than g0
                            #clunky way to code b/c doesn't allow nsp to vary
 gamma_g <- c(runif(nsp,0.2,1.25))  #species-specific rate of decline from max to min germ
-d <- g0 + (gmax-g0)*(1-exp(-gamma_g * xi[y]))  #this prob won't work - think about vector mult
+#d is the amount of germination in year y 
+d <- g0 + (gmax-g0)*(1-exp(-gamma_g * xi))  #this needs same matrix fussing (or could figure out the use of %*%)
 
 ##NEXT - model of germination g(t) determines time of entry into the model at what level
           
-h <-  rep(100,nsp)             # max rate of germination decrease following pulse
-
-
-#species-specific (ASK MEGAN! about the below onward ...)
-# ... as of meeting on 20 July 2021 may need to revisit edits here ... do we have a tau_c still etc.?
-
-tauc <-runif(nsp,0.01,0.2)
-taug <- runif(nsp,0.1,0.5)
-#okay ... I think ghat needs to be a matrix, similar to tauIhat...
-# tauIhat <- matrix(rep(tauI),nyrs,nsp, byrow = TRUE)
-ghat <- taug-tauc*xi
-
-g <- gmax*exp(-h*(matrix(rep(tauP,nsp),nrow=length(tauP),ncol=nsp)-tauIhat)^2)  #germination fraction in year y
-
-# Stuff we did not change ... 
-# g_max constant for all species (currently) -- leave as is
-# s, h, phi -- also leave as is
-# Rstar etc. stuff -- also leave as is
-
 ## Lizzie started editing this on 19 July 2021, but definitely not done ...
 
 #getSpecies characteristics
@@ -53,7 +36,6 @@ s <-  rep(0.8,nsp)      # seedbank survival overwinter
 #biomass conversion
 b <-  rep(1,nsp)          # biomass of seedling
 phi <- rep(0.05,nsp)     # conversion of end-of-season plant biomass to seeds
-
 
 #competition
 # c is conversion of resource to biomass; we vary this to vary R*
@@ -74,8 +56,8 @@ theta <- rep(1,nsp)      #nonlinearity in resource response
 m <-  rep(0.05,nsp)     # mortality
 Rstar <- (m/(a*(c-m*u)))^(1/theta)
 
-#concatenate all the parms to save
-sppvars <- as.data.frame(cbind(b, s, phi, a, u, c, m, theta, Rstar, gmax, h, alpha, tauI,tauIPini,tauIPns,tauIPfin))
+#concatenate all the species-specific parms to save (not the time-varying parms) 
+sppvars <- as.data.frame(cbind(b, s, phi, a, u, c, m, theta, Rstar, g0, gmax, gamma_g, tau_xi, tau_max))
 
 #write out species parameters at the beginning of each run;
 #  include headers if this is the first run
@@ -84,13 +66,13 @@ if (j==1) {
                             paste0(rep(names(sppvars),each=nsp),
                                    rep(c(1:nsp),times=length(sppvars))))
   col.names.EnvtParms <- c("jobID","arrayID","runID","yr",names(envtvars),
-                           paste0(rep("tauIhat",nsp),c(1:nsp)),
-                           paste0(rep("g",nsp),c(1:nsp)))
+                           paste0(rep("tau_g",nsp),c(1:nsp)),
+                           paste0(rep("d",nsp),c(1:nsp)))
 } else {
   col.names.SpeciesParms <- FALSE  
   col.names.EnvtParms <- FALSE
 }
-
+#this needs to be cleaned up so that it is writing out the new parameters
 write.table(matrix(data=c(as.numeric(jobID[1]),as.numeric(jobID[2]),j,as.matrix(sppvars)),nrow=1),
             file = paste0(OtherOut_loc,"/SpeciesParms",suffix),
             col.names = col.names.SpeciesParms,row.names = FALSE, 
