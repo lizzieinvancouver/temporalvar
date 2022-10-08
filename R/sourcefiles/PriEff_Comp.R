c28e86dc33a265d4020fa4254e16fcd13509c83d
donahuem <donahuem@hawaii.edu>
2022-10-07 18:15:32 -0600

    Removed the root function that stopped integration when R<Rstar. added forcing function for germination where Germinate1 and Germinate2 are functions that, given a time step, return the fraction of germination that takes place on that timestep. R, B1, B2 and tau_spr are all plotted in the ode graphical output. Moved the definition of Pars from here to PriEff_Model, so that it could be updated each year with a new germination timing

--- R/sourcefiles/PriEff_Comp.R
+++ R/sourcefiles/PriEff_Comp.R
@@ -6,34 +6,26 @@
 # https://tpetzoldt.github.io/deSolve-forcing/deSolve-forcing.html
 
 
-RstarComp <- function(Time,State,Pars, Germinate) {
-  gg <- Germinate(Time)
+RstarComp <- function(Time,State,Pars, Germinate1, Germinate2) {
+  g.spread.1 <- Germinate1(Time)
+  g.spread.2 <- Germinate2(Time)
   with(as.list(c(State,Pars)),{
     uptake1 <- a[1] * R^theta[1] / (1 + a[1] * u[1] * R^theta[1])
     uptake2 <- a[2] * R^theta[2] / (1 + a[2] * u[2] * R^theta[2])
     
     dR  <- -eps * R - B1 * uptake1 - B2 * uptake2 
-    dB1 <- (c[1] * uptake1 - m[1]) * B1 + gg[1]
-    dB2 <- (c[2] * uptake2 - m[2]) * B2 + gg[2]
+    dB1 <- (c[1] * uptake1 - m[1]) * B1 + (N0[1] * s[1] * g[1] * b[1])*g.spread.1
+    dB2 <- (c[2] * uptake2 - m[2]) * B2 + (N0[2] * s[2] * g[2] * b[2])*g.spread.2
     
-    return(list(c(dR,dB1,dB2)))
+    return(list(c(dR,dB1,dB2),tau_spr = c(g.spread.1,g.spread.2)))
     # dR = -eps*R - sum(B*a*R^theta/(1+a*u*R^theta))
     # dB = (c*a*R^theta/(1+a*u*R^theta)-m)*B
     # return(list(c(dR,dB)))
   })
 }
 
-##add root function so that the integration stops once (opt1) R is numerically 0 (threshold hh)
-rootfun <- function(Time, State, Pars) State[1] - min(Rstar)
+#Germination is a forcing function with new values each year
+#  g.spread will spread the germination over the growing season; sum(g.spread) = 1
+#  we will start with g.spread as all on one day (0 on all days except germ day=1)
 
-#Germination as a forcing function
-# start with each species just germinating once
-# create forcing function (ultimately, might want to do this in the _Species.R)
-Germinate <- data.frame(times = seq(0,days,1), gg = rep(0,days+1) )
-gunf <- approxfun(Germinate)
 
-#Define all parameters in the model
-Pars <- c(c=c,a=a,u=u,m=m,theta=theta,eps=eps)
-
-#Names for events
-Bnames <- c("B1","B2")
