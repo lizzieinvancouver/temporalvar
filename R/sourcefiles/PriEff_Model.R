b3480ee4d1e7b54a13b0eafe17087896b21ba6b1
donahuem <donahuem@hawaii.edu>
2022-10-07 18:18:48 -0600

    simplified N0 to B0 conversion removed germevents, which wasn't working added extended Pars, bc germination is more explicitly in the ode solver added two functions Germinate1, Germinate2 - these functions are, essentially, lookup functions for the proportion of the years germination occurring each time step. ind.Rstar is the day that R went less than R* for each species; equal to 120 (#days in a season) if it never went below R*

--- R/sourcefiles/PriEff_Model.R
+++ R/sourcefiles/PriEff_Model.R
@@ -8,32 +8,30 @@
 #  --> count seeds at N[y,]
 
 for (y in seq(1,nyrs)){
-  if(y==1) {
-    B0[y,] <- N0*s*g[y,]*b
-    } else {
-      B0[y,] <- N[y-1,]*s*g[y,]*b
-    }
+  if(y>1) N0<-N[y-1,]
+  B0[y,] <- N0*s*g[y,]*b
   R <- R0[y]
   B1 <- 0
   B2 <- 0
   State <- c(R=R,B1=B1,B2=B2)
-  Time <- seq(0,days,by=1)
-  tau.index <- c(which.min(tau_g[y,]),which.max(tau_g[y,]))
-  germevents <- data.frame(var = Bnames[tau.index],
-                           time = tau_g[y,tau.index],
-                           value = B0[y,tau.index], 
-                           method = c("add", "add"))
-  
-  Bout[[y]] <- as.data.frame(ode(func = RstarComp,
+  Time <- seq(0,days,by=dt)
+  Pars <- c(c=c,a=a,u=u,m=m,theta=theta,eps=eps,s=s, g=g[y,],b=b,N0=N0)
+  Germinate1 <-approxfun(tau_spr[[y]][[1]], method = "constant",rule=2)
+  Germinate2 <-approxfun(tau_spr[[y]][[2]], method = "constant",rule=2)
+  Bout[[y]] <- ode(func = RstarComp,
                            y = State,
                            parms = Pars,
                            times = Time, 
-                           events=list(data=germevents),
-                           rootfun=rootfun))
+                           Germinate1 = Germinate1,
+                           Germinate2 = Germinate2)
+  #step when R went below R* for each species
+  ind.Rstar<- c(min(which(Bout[[y]][,"R"]<Rstar[1]),120),min(which(Bout[[y]][,"R"]<Rstar[2]),120))
+  print(ind.Rstar)
   source(here("R","sourcefiles","PriEff_PlotSeason.R"))
 
   #iterate N: seeds that survived but did not germinate + new seeds
-  Bfin[y,] <- c(tail(Bout[[y]]$B1,1),tail(Bout[[y]]$B2,1))
+  #get Bfin the first step where R<R*min
+  Bfin[y,] <- c(Bout[[y]][,"B1"][ind.Rstar[1]],Bout[[y]][,"B2"][ind.Rstar[2]])
   N[y,] <- Bfin[y,]*phi + ifelse(y==1,N0*s*(1-g[y,]), N[y-1,]*s*(1-g[y,]))  
   N[y,] <- N[y,]*(N[y,] > ext)            # call very low densities true zero
   if(isFALSE(sum(N)>0)) break             # if all species have gone extinct, stop
