057321734a0adae59edec6bcdd754aa5d4de093b
donahuem <donahuem@hawaii.edu>
2022-10-26 19:00:41 -0600

    Updated how germination enters the model.  Now g is the fraction of germination each day for each species

--- R/sourcefiles/PriEff_Comp.R
+++ R/sourcefiles/PriEff_Comp.R
@@ -7,17 +7,17 @@
 
 
 RstarComp <- function(Time,State,Pars, Germinate1, Germinate2) {
-  g.spread.1 <- Germinate1(Time)
-  g.spread.2 <- Germinate2(Time)
+  g1 <- Germinate1(Time)
+  g2 <- Germinate2(Time)
   with(as.list(c(State,Pars)),{
     uptake1 <- a[1] * R^theta[1] / (1 + a[1] * u[1] * R^theta[1])
     uptake2 <- a[2] * R^theta[2] / (1 + a[2] * u[2] * R^theta[2])
     
     dR  <- -eps * R - B1 * uptake1 - B2 * uptake2 
-    dB1 <- (c[1] * uptake1 - m[1]) * B1 + (N0[1] * s[1] * g[1] * b[1])*g.spread.1
-    dB2 <- (c[2] * uptake2 - m[2]) * B2 + (N0[2] * s[2] * g[2] * b[2])*g.spread.2
+    dB1 <- (c[1] * uptake1 - m[1]) * B1 + (N0[1] * s[1] * b[1])*g1
+    dB2 <- (c[2] * uptake2 - m[2]) * B2 + (N0[2] * s[2] * b[2])*g2
     
-    return(list(c(dR,dB1,dB2),tau_spr = c(g.spread.1,g.spread.2)))
+    return(list(c(dR,dB1,dB2),g = c(g1,g2)))
     # dR = -eps*R - sum(B*a*R^theta/(1+a*u*R^theta))
     # dB = (c*a*R^theta/(1+a*u*R^theta)-m)*B
     # return(list(c(dR,dB)))
@@ -25,7 +25,7 @@ RstarComp <- function(Time,State,Pars, Germinate1, Germinate2) {
 }
 
 #Germination is a forcing function with new values each year
-#  g.spread will spread the germination over the growing season; sum(g.spread) = 1
-#  we will start with g.spread as all on one day (0 on all days except germ day=1)
-
+#  g1,g2 spread the germination over the growing season
+#  version 1 puts all germination on day tau_g50, gmax[yr,sp]
+#  version 2 spreads germination by Hill Eqn with g50 at tau_g50
 
