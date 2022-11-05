b7bd8f9ff42bbc0641c76af39a4855b12a41ac6e
donahuem <donahuem@hawaii.edu>
2022-11-04 22:55:47 -0600

    Started thinking abuot a wrapper for output, but didn't finish this yet

--- R/PriorityEffects.R
+++ R/PriorityEffects.R
@@ -7,24 +7,30 @@ library(here)
 here()
 
 #define the run - Consider creating a dataframe with combinations of parms to test
-nyrs <- 50
 
-#define the environment for this run
-source(here("R","sourcefiles","PriEff_Envt.R"))
+for (n in c(1,nruns)) {
 
-#define the species in this run
-source(here("R","sourcefiles","PriEff_Species.R"))
+  nyrs <- 300
+  
+  #define the environment for this run
+  source(here("R","sourcefiles","PriEff_Envt.R"))
+  
+  #define the species in this run
+  source(here("R","sourcefiles","PriEff_Species.R"))
+  
+  #run the model for nyrs
+  source(here("R","sourcefiles","PriEff_Comp.R"))
+  source(here("R","sourcefiles","PriEff_Model.R"))
 
-#run the model for nyrs
-source(here("R","sourcefiles","PriEff_Comp.R"))
-source(here("R","sourcefiles","PriEff_Model.R"))
+  # #write out the results for this run
+  # source(here("R","sourcefiles","PriEff_Output.R"))
+  
+}
 
-
-
-#plot for testing
-par(mfrow=c(1,1))
-plot(seq(1,nyrs,dt),Bfin[,2],col="blue")
-points(seq(1,nyrs,dt),Bfin[,1],col="red")
-
+# #plot for testing
+# par(mfrow=c(1,1))
+# plot(seq(1,nyrs,dt),Bfin[,2],col="blue")
+# points(seq(1,nyrs,dt),Bfin[,1],col="red")
+# 
 
 
